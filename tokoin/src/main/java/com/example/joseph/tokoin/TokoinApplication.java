1a79aabcae0d115a8fc4db0586003d2623cab68d
Joseph <nguld@itsj-group.com>
2020-12-26 21:28:20 +0700

    init commit

---
+++ tokoin/src/main/java/com/example/joseph/tokoin/TokoinApplication.java
@@ -0,0 +1,13 @@
+package com.example.joseph.tokoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokoinApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TokoinApplication.class, args);
+  }
+
+}
---
+++ tokoin/src/test/java/com/example/joseph/tokoin/TokoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joseph.tokoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokoinApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
